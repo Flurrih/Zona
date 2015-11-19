f99d5baff2f35eeec5c1ec3f3e5a4a1379d2f6a1
Flurrih <ghost744@gmail.com>
2015-11-19 01:22:44 +0100

    Added prototype of inventory - still work in progress

---
+++ Assets/Scripts/inventory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inventory : MonoBehaviour {
+    private int numberSlots;
+    private item[] inventoryArray;
+
+	// Use this for initialization
+	void Start () {
+        inventoryArray = new item[numberSlots];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void addItem(int item_id)
+    {
+        inventoryArray[item_id - 1].own();
+    }
+
+    public void useItem(int item_id)
+    {
+        inventoryArray[item_id - 1].use();
+    }
+}
---
+++ Assets/Scripts/item.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class item : MonoBehaviour {
+    public int id;
+    public int quantity;
+    public string itemName;
+    public string description;
+    private bool isUsed;
+    private bool owned;
+	// Use this for initialization
+	void Start () {
+        isUsed = false;
+        owned = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void own()
+    {
+        owned = true;
+    }
+
+    public void use()
+    {
+        if(quantity > 0)
+        {
+            isUsed = true;
+            quantity--;
+            //do something
+        }
+        
+    }
+}
